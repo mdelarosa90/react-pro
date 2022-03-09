7329212f0e6da3cf6260b9b475d5217e807c0986
Michel De La Rosa <micheldelarosa@blacksip.com>
2022-03-09 08:43:02 -0500

    Base Proyecto

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom";
+import logo from '../logo.svg';
+
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <nav>
+          <img src={logo} alt="React Logo" />
+          <ul>
+            <li>
+              <NavLink to="/home" className={({isActive}) => isActive ? 'nav-active': ""} end>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" className={({isActive}) => isActive ? 'nav-active': ""} end>About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/users" className={({isActive}) => isActive ? 'nav-active': ""} end>Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/home" element={<h1>Home Page</h1>} />
+          <Route path="/about" element={<h1>About Page</h1>} />
+          <Route path="/users" element={<h1>Users Page</h1>} />
+          <Route path="/*" element={<Navigate to="/home" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  )
+}
