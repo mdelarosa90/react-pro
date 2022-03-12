0ccd9ec5e1fbbc48c1b1b38ffa2f3caaed451222
Michel De La Rosa <micheldelarosa@blacksip.com>
2022-03-12 16:51:08 -0500

    Control Props alternativo

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -5,31 +5,18 @@ export const useShoppingCart = () => {
   const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
   const onProductCountChange = ({ count, product }: { count: number, product: Product }) => {
     //console.log('onProductCountChange', count, product)
-    setShoppingCart((oldShoppingCart) => {
-      const productInCart: ProductInCart = oldShoppingCart[product.id] || {...product, count: 0};
-      if(Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
-        return {
-          ...oldShoppingCart,
-          [product.id]: productInCart
-        }
+    
+    setShoppingCart(oldShoppingCart => {
+      if (count === 0) {
+        // let newShoppingCart = {...oldShoppingCart};
+        // delete newShoppingCart[product.id];
+        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+        return rest;
       }
-
-      // Borrar Producto
-      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      return rest;
-
-      // if (count === 0) {
-      //   let newShoppingCart = {...oldShoppingCart};
-      //   delete newShoppingCart[product.id];
-      //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-      //   return rest;
-      // }
-
-      // return {
-      //   ...oldShoppingCart,
-      //   [product.id]: { ...product, count }
-      // }
+      return {
+        ...oldShoppingCart,
+        [product.id]: { ...product, count }
+      }
     })
   }
 
